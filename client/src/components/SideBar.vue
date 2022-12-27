ffb61d51ea8dfa8bd8cae87bbb4805cbb67e3277
Gabriel <gabrielferrari601@gmail.com>
2022-12-27 16:54:26 -0300

    feat(client): :sparkles: add the sidebar to the main page

---
+++ client/src/components/SideBar.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section>
+    <img src="src/assets/menu-hero.svg" class="hero-icon" alt="cash force" />
+    <div class="nav-option-container">
+      <span/>
+      <img src="src/assets/nficon.svg" alt="notas fiscais" class="menu-icon">
+      <p class="p-medium">Notas fiscais</p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  section {
+    height: 100vh;
+    width: 20%;
+    background-color: #FBFDFE;
+  }
+
+  .hero-icon {
+    width: 18em;
+    height: 5em;
+  }
+
+  .menu-icon {
+    width: 2rem;
+    height: 0.875rem;
+  }
+
+  .nav-option-container {
+    display: flex;
+    align-items: center;
+    height: 4rem;
+    padding: 8px 24px;
+    position: absolute;
+  }
+
+  .nav-option-container:hover span {
+    width: 0.25rem;
+    background-color: #00AD8C;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    right: 0;
+    border-radius: 10px;
+    transition: 0.5s;
+  }
+
+  .nav-option-container:hover .p-medium {
+    color: #00AD8C;
+  }
+
+  .nav-option-container:hover .menu-icon {
+    fill: #00AD8C;
+  }
+
+</style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/global.css'
+import './assets/typography.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
